1dc81a2edf85bbfa3589095f4e74e99a9d5f1166
duskembayev <d.uskembayev@gmail.com>
2023-08-17 21:23:24 +0300

    introduce user & role models

---
+++ src/cryptobank.api/dal/users/Role.cs
@@ -0,0 +1,7 @@
+﻿namespace cryptobank.api.dal.users;
+
+public class Role
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+}
---
+++ src/cryptobank.api/dal/users/User.cs
@@ -0,0 +1,12 @@
+﻿namespace cryptobank.api.dal.users;
+
+public class User
+{
+    public int Id { get; init; }
+    public string Email { get; init; } = string.Empty;
+    public string PasswordHash { get; init; } = string.Empty;
+    public string PasswordSalt { get; init; } = string.Empty;
+    public DateOnly DateOfBirth { get; init; } = DateOnly.MinValue;
+    public DateTime DateOfRegistration { get; init; } = DateTime.UnixEpoch;
+    public List<Role> Roles { get; } = new();
+}
