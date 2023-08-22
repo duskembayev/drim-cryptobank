4c3d256b7a4595bc0659a66c79dede961d3c28ef
duskembayev <d.uskembayev@gmail.com>
2023-08-22 15:11:39 +0300

    cleanup

--- src/cryptobank.api/features/users/services/AccessTokenProvider.cs
+++ src/cryptobank.api/features/users/services/AccessTokenProvider.cs
@@ -11,9 +11,11 @@ namespace cryptobank.api.features.users.services;
 public class AccessTokenProvider : IAccessTokenProvider
 {
     private const string SecurityAlgorithm = SecurityAlgorithms.HmacSha256;
+
     private readonly JsonWebTokenHandler _jsonWebTokenHandler;
     private readonly ITimeProvider _timeProvider;
     private readonly IOptions<AccessTokenOptions> _options;
+    private readonly SecurityKey _securityKey;
 
     public AccessTokenProvider(
         JsonWebTokenHandler jsonWebTokenHandler,
@@ -23,30 +25,39 @@ public class AccessTokenProvider : IAccessTokenProvider
         _jsonWebTokenHandler = jsonWebTokenHandler;
         _timeProvider = timeProvider;
         _options = options;
+
+        _securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Value.SecurityKey));
     }
 
     public Task<string> IssueAsync(User user, CancellationToken cancellationToken)
     {
         var utcNow = _timeProvider.UtcNow;
+
         var securityTokenDescriptor = new SecurityTokenDescriptor
         {
             Issuer = _options.Value.Issuer,
             Audience = _options.Value.Audience,
-            Subject = new ClaimsIdentity(new List<Claim>
-            {
-                new(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
-                new(JwtRegisteredClaimNames.Email, user.Email),
-                new("roles", user.Roles.Aggregate(Roles.None, (roles, role) => roles | (Roles) role.Id).ToString("D")),
-            }, "Bearer"),
-            SigningCredentials = new SigningCredentials(
-                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Value.SecurityKey)),
-                SecurityAlgorithm),
             Expires = utcNow.Add(_options.Value.Expiration),
             IssuedAt = utcNow,
             NotBefore = utcNow,
+            Subject = GetClaims(user),
+            SigningCredentials = new SigningCredentials(_securityKey, SecurityAlgorithm),
         };
 
         var token = _jsonWebTokenHandler.CreateToken(securityTokenDescriptor);
         return Task.FromResult(token);
+    }
+
+    private static ClaimsIdentity GetClaims(User user)
+    {
+        var claims = new ClaimsIdentity("Bearer");
+
+        claims.AddClaim(new Claim("id", user.Id.ToString()));
+        claims.AddClaim(new Claim("email", user.Email));
+
+        foreach (var role in user.Roles)
+            claims.AddClaim(new Claim("roles", role.Name));
+
+        return claims;
     }
 }
