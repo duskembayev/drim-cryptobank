cfd7bf424a94a4d8bad216750b0c49f6fc0a5b94
duskembayev <d.uskembayev@gmail.com>
2023-08-24 22:28:05 +0300

    use argon2

--- src/cryptobank.api/features/users/SetupExtensions.cs
+++ src/cryptobank.api/features/users/SetupExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using cryptobank.api.features.users.config;
 using Microsoft.IdentityModel.JsonWebTokens;
+using PasswordOptions = Microsoft.AspNetCore.Identity.PasswordOptions;
 
 namespace cryptobank.api.features.users;
 
@@ -7,12 +8,14 @@ public static class SetupExtensions
 {
     private const string RegisterUserSectionKey = "Features:Users:RegisterUser";
     private const string AccessTokenSectionKey = "Features:Users:AccessToken";
+    private const string PasswordSectionKey = "Features:Users:Password";
 
     public static WebApplicationBuilder AddUsers(this WebApplicationBuilder @this)
     {
         @this.Services
             .Configure<RegisterUserOptions>(@this.Configuration.GetSection(RegisterUserSectionKey))
             .Configure<AccessTokenOptions>(@this.Configuration.GetSection(AccessTokenSectionKey))
+            .Configure<PasswordOptions>(@this.Configuration.GetSection(PasswordSectionKey))
             .AddSingleton<JsonWebTokenHandler>();
 
         return @this;
--- src/cryptobank.api/features/users/config/RegisterUserOptions.cs
+++ src/cryptobank.api/features/users/config/RegisterUserOptions.cs
@@ -3,7 +3,5 @@
 public class RegisterUserOptions
 {
     public string? FallbackAdminEmail { get; set; }
-    public int PasswordSaltSize { get; set; } = 64;
-    public int PasswordHashSize { get; set; } = 128;
     public int MinimumAge { get; set; } = 16;
 }
