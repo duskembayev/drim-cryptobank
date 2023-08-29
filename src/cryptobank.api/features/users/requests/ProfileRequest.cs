44427e470cb226c726f6e6d1fe2d842409cca684
duskembayev <d.uskembayev@gmail.com>
2023-08-29 17:13:42 +0300

    get rid of UserProvider

--- src/cryptobank.api/features/users/requests/ProfileRequest.cs
+++ src/cryptobank.api/features/users/requests/ProfileRequest.cs
@@ -1,8 +1,10 @@
 ﻿using cryptobank.api.features.users.responses;
+using cryptobank.api.features.users.services;
 
 namespace cryptobank.api.features.users.requests;
 
 public class ProfileRequest : IRequest<ProfileResponse>
 {
+    [FromClaim(AccessTokenConstants.ClaimsTypes.Id)]
     public int UserId { get; set; }
 }
--- src/cryptobank.api/utils/security/IUserProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace cryptobank.api.utils.security;
-
-public interface IUserProvider
-{
-    public int Id { get; }
-    public string Email { get; }
-    public IEnumerable<string> Roles { get; }
-}
