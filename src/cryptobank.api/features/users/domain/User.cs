41cb6b5bd86e6dff05568ff9380a2def96cd78dd
duskembayev <d.uskembayev@gmail.com>
2023-08-29 14:56:27 +0300

    fix merge error

--- src/cryptobank.api/features/users/domain/User.cs
+++ src/cryptobank.api/features/users/domain/User.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using cryptobank.api.core;
+using cryptobank.api.features.accounts.domain;
+
 namespace cryptobank.api.features.users.domain;
 
 public class User
 {
     public static readonly User Empty = new();
-    
+
     public int Id { get; init; }
     public string Email { get; init; } = string.Empty;
     public string PasswordHash { get; init; } = string.Empty;
