ad0c7b65d83273e20770de8edb639a99eb27df9b
duskembayev <d.uskembayev@gmail.com>
2023-09-01 17:41:23 +0300

    use ISystemClock in TimeProvider

--- src/cryptobank.api/utils/environment/TimeProvider.cs
+++ src/cryptobank.api/utils/environment/TimeProvider.cs
@@ -1,8 +1,17 @@
-﻿namespace cryptobank.api.utils.environment;
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace cryptobank.api.utils.environment;
 
 [ContainerEntry(ServiceLifetime.Singleton, typeof(ITimeProvider))]
 internal sealed class TimeProvider : ITimeProvider
 {
-    public DateOnly Today => DateOnly.FromDateTime(DateTime.Today);
-    public DateTime UtcNow => DateTime.UtcNow;
+    private readonly ISystemClock _systemClock;
+
+    public TimeProvider(ISystemClock systemClock)
+    {
+        _systemClock = systemClock;
+    }
+
+    public DateOnly Today => DateOnly.FromDateTime(_systemClock.UtcNow.UtcDateTime);
+    public DateTime UtcNow => _systemClock.UtcNow.UtcDateTime;
 }
