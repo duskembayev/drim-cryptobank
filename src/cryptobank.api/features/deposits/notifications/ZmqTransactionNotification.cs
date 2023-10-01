3ec5c5e1b9d0652d0b52a3660e9801069e622ad0
duskembayev <d.uskembayev@gmail.com>
2023-10-01 12:07:43 +0300

    introduce zmq subscription & notifications

---
+++ src/cryptobank.api/features/deposits/notifications/ZmqTransactionNotification.cs
@@ -0,0 +1,18 @@
+﻿using NBitcoin;
+
+namespace cryptobank.api.features.deposits.notifications;
+
+/// <summary>
+/// https://github.com/bitcoin/bitcoin/blob/master/doc/zmq.md#rawtx
+/// </summary>
+internal sealed class ZmqTransactionNotification : INotification
+{
+    private readonly byte[] _body;
+
+    public ZmqTransactionNotification(byte[] body)
+    {
+        _body = body;
+    }
+
+    public Transaction ToTransaction(Network network) => Transaction.Load(_body, network);
+}
